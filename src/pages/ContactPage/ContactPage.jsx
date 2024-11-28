2aa34b6f9def0ea371a9aa5f12137e143b2164da
Andrew Flanagan <andrewflanagan@Andrews-MacBook-Air.local>
2024-11-27 17:37:57 -0800

    finish contact page cosmetically, need to do functionality

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Typography, Button, Container } from "@mui/material";
+import { Box, Typography, Button, Container, TextField } from "@mui/material";
 import { Link } from "react-router-dom";
 import '../../styles/global.css';
 
@@ -12,12 +12,34 @@ const ContactPage = () => {
                     Contact Us
                 </Typography>
                 <Typography variant="h5" align="center">
-                    Have a question? Reach out to us!
+                    Have a question? Reach out to the Chad!
                 </Typography>
                 <Box sx={{ display: "flex", justifyContent: "center", mt: 4 }}>
-                    <Button component={Link} to="/search" variant="contained" size="large">
-                        Browse Movies
-                    </Button>
+                    <TextField 
+                        fullWidth 
+                        label="Name"
+                        required
+                        sx={{display: "inline-block", marginRight: "16px"}}>
+                    </TextField>
+                    <TextField 
+                        fullWidth
+                        label="Email"
+                        required
+                        sx={{display: "inline-block"}}>
+                    </TextField>
+                </Box>
+                <TextField 
+                    fullWidth
+                    label="Message"
+                    multiline
+                    minRows={3}
+                    sx={{margin: "auto", mt: 1, '& .MuiInputBase-root': {
+                        height: "30vh", alignItems: "flex-start", textOverflow: "ellipsis"}}}
+                    required
+                >
+                </TextField>
+                <Box sx={{display: "flex"}}>
+                    <Button variant="outlined" size="large" sx={{marginLeft: "auto", mt: 1, bgcolor: "#1976d2", color: "#FEE440"}}>Send</Button>
                 </Box>
             </Container>
         </Box>
