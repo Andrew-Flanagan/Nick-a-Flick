86d8b6cee261cd8f20742b32c25cab07727107be
Andrew Flanagan <andrewflanagan@Andrews-MacBook-Air.local>
2024-12-01 16:11:48 -0800

    add some error handling for contact form

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -5,6 +5,19 @@ import '../../styles/global.css';
 
 
 const ContactPage = () => {
+    const [name, setName] = React.useState("");
+    const [email, setEmail] = React.useState("");
+    const [message, setMessage] = React.useState("");
+    const [submitted, setSubmitted] = React.useState(false);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(name, email, message);
+        setSubmitted(true);
+        
+    }
+
+
     return (
         <Box sx={{py: 8}}>
             <Container>
@@ -19,27 +32,38 @@ const ContactPage = () => {
                         fullWidth 
                         label="Name"
                         required
-                        sx={{display: "inline-block", marginRight: "16px"}}>
+                        sx={{display: "inline-block", marginRight: "16px"}}
+                        onChange={(event) => setName(event.target.value)}
+                        value={name}
+                        error={name.length === 0 && submitted}
+                    >
                     </TextField>
                     <TextField 
                         fullWidth
                         label="Email"
                         required
-                        sx={{display: "inline-block"}}>
+                        sx={{display: "inline-block"}}
+                        onChange={(event) => setEmail(event.target.value)}
+                        value={email}
+                        error={email.length === 0 && submitted}
+                    >
                     </TextField>
                 </Box>
                 <TextField 
                     fullWidth
                     label="Message"
                     multiline
-                    minRows={3}
+                    slotProps={{ htmlInput: { maxLength: 1000 } }}
                     sx={{margin: "auto", mt: 1, '& .MuiInputBase-root': {
-                        height: "30vh", alignItems: "flex-start", textOverflow: "ellipsis"}}}
+                        height: "30vh", alignItems: "flex-start", textOverflow: "ellipsis", overflow: 'scroll', flexGrow: 1}}}
                     required
+                    onChange={(event) => setMessage(event.target.value)}
+                    value={message}
+                    error={message.length === 0 && submitted}
                 >
                 </TextField>
                 <Box sx={{display: "flex"}}>
-                    <Button variant="outlined" size="large" sx={{marginLeft: "auto", mt: 1, bgcolor: "#1976d2", color: "#FEE440"}}>Send</Button>
+                    <Button variant="outlined" type="submit" size="large" sx={{marginLeft: "auto", mt: 1, bgcolor: "#1976d2", color: "#FEE440"}} onClick={handleSubmit}>Send</Button>
                 </Box>
             </Container>
         </Box>
