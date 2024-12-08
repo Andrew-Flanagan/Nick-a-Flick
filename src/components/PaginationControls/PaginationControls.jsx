103937b5505848841e1a756b7af2d94229d1ec0a
Andrew-Flanagan <andrewflanagan97@gmail.com>
2024-12-08 13:37:44 -0800

    improve pagination

--- src/components/PaginationControls/PaginationControls.jsx
+++ src/components/PaginationControls/PaginationControls.jsx
@@ -4,6 +4,7 @@ import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 const PaginationControls = ({ page, totalResults, numResults, onPageChange, genre, genres }) => {
   const totalPages = Math.ceil(totalResults / numResults);
+  const pageArray = Array.from({ length: totalPages }, (_, i) => i + 1);
 
   const handlePrevPage = () => {
     if (page > 1) {
@@ -17,20 +18,63 @@ const PaginationControls = ({ page, totalResults, numResults, onPageChange, genr
     }
   };
 
+  const getPageButtons = (pageArray) => {
+    const isMobile = window.innerWidth <= 600;
+
+    if (isMobile) {
+      return [];
+    }
+
+
+    if (pageArray.length <= 5) {
+      return pageArray;
+    }
+
+    const curr_array = pageArray.slice(Math.max(page - 3, 0), Math.min(page + 2, totalPages));
+    // Add ellipsis to the left
+    if (curr_array[0] > 1) {
+      curr_array.unshift("...");
+    }
+    // Add ellipsis to the right
+    if (curr_array[curr_array.length - 1] < totalPages) {
+      curr_array.push("...");
+    }
+    // add first and last page
+    if (page >= 4) {
+      curr_array.unshift(1);
+    }
+    if (page <= totalPages - 3) {
+      curr_array.push(totalPages);
+    }
+    
+    return curr_array;
+  };
+
+  const getVariant = (p) => {
+    if (p === page) {
+      return "contained";
+    }
+    if (p === "...") {
+      return "text";
+    }
+    return "outlined";
+  };
+
   return (
-    <Box sx={{ display: "flex", alignItems: "center", justifyContent: "center", marginBottom: "15px", gap: "1rem", marginTop: "15px" }}>
-      <Typography variant="h6" align="center" gutterBottom>
-        Page {page} of {totalPages}. {totalResults} total{" "}
-        {genre ? genres.find((g) => g.id === genre)?.name.toLocaleLowerCase() : "movies and tv"}.
-      </Typography>
-      <ButtonGroup variant="contained" aria-label="Basic button group" size="small" sx={{ height: "56px" }}>
-        <Button onClick={handlePrevPage} startIcon={<ArrowBackIosIcon />} variant="contained" disabled={page === 1}>
-          Prev Page
+    <Box sx={{ display: "flex", alignItems: "center", justifyContent: "space-between", gap: "1rem", margin: "15px 0px 15px 0px" }}>
+      <Button onClick={handlePrevPage} startIcon={<ArrowBackIosIcon />} variant="contained" disabled={page === 1} sx={{float: "left"}}>
+          Prev
         </Button>
+      <ButtonGroup>
+        {getPageButtons(pageArray).map((p) => (
+          <Button key={p} disabled={!Number.isInteger(p)} variant={getVariant(p)} onClick={() => onPageChange(p)}>
+            {p}
+          </Button>
+        ))}
+      </ButtonGroup>        
         <Button onClick={handleNextPage} endIcon={<ArrowForwardIosIcon />} variant="contained" disabled={page === totalPages}>
-          Next Page
+          Next
         </Button>
-      </ButtonGroup>
     </Box>
   );
 };
